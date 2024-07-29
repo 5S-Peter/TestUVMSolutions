31a83fe4b089b046cb1d0fcd5b13dd0bbf9271f1
Peter Simon <peter@5.systems>
2024-07-29 21:06:16 +0200

    moved test to proper folder

--- s5s_dpr/example/file_list/compile.f
+++ s5s_dpr/example/file_list/compile.f
@@ -2,7 +2,7 @@
 -f ${S5S_UVM_TEST}/s5s_dpr/file_list/compile.f
 
 +incdir+${S5S_UVM_TEST}/s5s_dpr/example/sv/env
-+incdir+${S5S_UVM_TEST}/s5s_dpr/example/sv/test
++incdir+${S5S_UVM_TEST}/s5s_dpr/example/sv/tests
 
 ${S5S_UVM_TEST}/s5s_dpr/example/sv/env/example_dpr_pkg.sv
 ${S5S_UVM_TEST}/s5s_dpr/example/sv/tb/top.sv
